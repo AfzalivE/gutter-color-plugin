d9a1dd37da9ef43f58278ae30d5bfe129d6e8196
GitHub Action <action@github.com>
2020-10-21 00:35:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/afzalive/guttercolorplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.afzalive.guttercolorplugin.services
+
+import com.github.afzalive.guttercolorplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
